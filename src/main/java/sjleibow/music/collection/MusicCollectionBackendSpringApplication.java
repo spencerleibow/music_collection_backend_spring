ad59bba44c56cbad767d6466f9631deeae57a9e1
spencerleibow <spencerleibow@10.0.1.11>
2018-05-16 01:17:09 -0700

    Initial Spring Boot project

---
+++ src/main/java/sjleibow/music/collection/MusicCollectionBackendSpringApplication.java
@@ -0,0 +1,12 @@
+package sjleibow.music.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCollectionBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCollectionBackendSpringApplication.class, args);
+	}
+}
